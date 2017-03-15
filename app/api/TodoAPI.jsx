776ba5c90865e7d2d91f89b9b2ad0175cabff3b0
websitekay <websitekay@gmail.com>
2017-03-15 00:08:00 -0700

    Filter todos by showCompleted

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,19 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : []
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted
+    })
+
+    // filter by searchText
+
+    // sort todos with non-completed first
+
+    return filteredTodos
   }
 }
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -51,4 +51,30 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos)
     })
   })
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Some text',
+      completed: true
+    }, {
+      id: 2,
+      text: 'Other text',
+      completed: false
+    }, {
+      id: 3,
+      text: 'Some more text',
+      completed: true
+    }]
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+      expect(filteredTodos.length).toBe(3)
+    })
+
+    it('should return non-completed items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '')
+      expect(filteredTodos.length).toBe(1)
+    })
+  })
 })
